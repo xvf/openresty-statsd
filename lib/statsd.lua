5c4de8e445f40fbda66e041b5ec6183fe4d68c74
xvf <abi.srivastava@namely.com>
2018-03-29 13:27:19 -0400

    added namespace

--- lib/statsd.lua
+++ lib/statsd.lua
@@ -1,8 +1,21 @@
 local Statsd = {}
 
-Statsd.time  = function (bucket, time) Statsd.register(bucket, time .. "|ms") end
-Statsd.count = function (bucket, n)    Statsd.register(bucket, n .. "|c") end
-Statsd.incr  = function (bucket)       Statsd.count(bucket, 1) end
+local namespace = os.getenv("STATS_NAMESPACE") or "openresty"
+
+Statsd.time  = function (metric, time)
+   metric_name = namespace .. ".timer." .. metric
+   Statsd.register(metric_name , time .. "|ms")
+end
+
+Statsd.count = function (metric, n)
+   metric_name = namespace .. ".count." .. metric
+   Statsd.register(metric_name,  n .. "|c")
+end
+
+Statsd.incr  = function (metric)
+   metric_name = namespace .. ".counter." .. metric
+   Statsd.count(metric_name, 1 .. "|c")
+end
 
 Statsd.buffer = {} -- this table will be shared per worker process
                    -- if lua_code_cache is off, it will be cleared every request
@@ -16,13 +29,15 @@ Statsd.flush = function(sock, host, port)
                udp:close()
             end)
    end
-   
+
    -- empty buffer
    for k in pairs(Statsd.buffer) do Statsd.buffer[k] = nil end
 end
 
-Statsd.register = function (bucket, suffix, sample_rate)
-   table.insert(Statsd.buffer, bucket .. ":" .. suffix .. "\n")
+Statsd.register = function (metric, suffix)
+   local _metric = metric  .. ":" .. suffix .. "\n"
+   table.insert(Statsd.buffer, _metric)
+
 end
 
-return Statsd 
+return Statsd
